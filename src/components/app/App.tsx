3b2eaf994500ec053693bb88036978b26c7f255c
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-08-09 13:41:45 +0300

    fix: fix login and signup errors

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,19 +1,17 @@
 import './App.scss';
 import { useState } from 'react';
-import { createApiCustomerRoot, login, signup } from '../../utils/libs/sdk/customerCallFunctions';
-import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
-import { anonymousApiRoot } from '../../utils/libs/sdk/BuildClients';
+import ApiRoot from '../../utils/libs/sdk/ApiRoot';
 
 function App() {
-  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [customer, setCustomer] = useState<ByProjectKeyRequestBuilder>(anonymousApiRoot);
+  const [user, setUser] = useState({ apiRoot: new ApiRoot(), isLoggedIn: false });
 
   return (
     <form className="add-form">
       <div className="form-control">
         <label>Name</label>
-        <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="name" value={username} onChange={(e) => setUsername(e.target.value)} />
       </div>
       <div className="form-control">
         <label>Password</label>
@@ -23,12 +21,44 @@ function App() {
         className="btn btn-block"
         type="button"
         value="Login"
-        onClick={() => {
-          setCustomer(createApiCustomerRoot(name, password));
-          login(customer, name, password);
+        onClick={async () => {
+          try {
+            await user.apiRoot.login(username, password);
+            setUser({ apiRoot: new ApiRoot({ username, password }), isLoggedIn: true });
+          } catch {
+            console.log('error');
+          }
         }}
       />
-      <input className="btn btn-block" type="button" value="Sign up" onClick={() => signup(customer, name, password)} />
+      <input
+        className="btn btn-block"
+        type="button"
+        value="Sign up"
+        onClick={async () => {
+          user.apiRoot
+            .signup(username, password)
+            .then(() => {
+              setUser({ apiRoot: new ApiRoot({ username, password }), isLoggedIn: true });
+            })
+            .catch((err) => console.log(err.message));
+        }}
+      />
+      <input
+        className="btn btn-block"
+        type="button"
+        value="Logout"
+        onClick={async () => {
+          setUser({ apiRoot: new ApiRoot(), isLoggedIn: false });
+        }}
+      />
+      <input
+        className="btn btn-block"
+        type="button"
+        value="getUser"
+        onClick={async () => {
+          user.apiRoot.apiRoot.me().get().execute();
+        }}
+      />
     </form>
   );
 }
--- src/utils/libs/sdk/BuildClients.ts
+++ src/utils/libs/sdk/BuildClients.ts
@@ -1,32 +1,80 @@
 import {
   ClientBuilder,
+  type Client,
+  type Credentials,
   type HttpMiddlewareOptions,
   type AnonymousAuthMiddlewareOptions,
+  type UserAuthOptions,
+  type PasswordAuthMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
 
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 
-const authAnonymousMiddlewareOptions: AnonymousAuthMiddlewareOptions = {
-  host: process.env.REACT_APP_AUTH_URL as string,
-  projectKey: process.env.REACT_APP_PROJECT_KEY as string,
-  credentials: {
+const userClientBuilder = new ClientBuilder();
+const anonymousClientBuilder = new ClientBuilder();
+
+class CtpClient {
+  private projectKey: string = process.env.REACT_APP_PROJECT_KEY as string;
+  private oauthURL: string = process.env.REACT_APP_AUTH_URL as string;
+  private apiURL: string = process.env.REACT_APP_API_URL as string;
+  private credentials: Credentials = {
     clientId: process.env.REACT_APP_CLIENT_ID as string,
     clientSecret: process.env.REACT_APP_CLIENT_SECRET as string,
-  },
-  fetch,
-};
+  };
+  private userAuthOptions?: UserAuthOptions;
 
-const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: process.env.REACT_APP_API_URL as string,
-  fetch,
-};
+  constructor(userAuthOptions?: UserAuthOptions) {
+    if (userAuthOptions) this.userAuthOptions = userAuthOptions;
+  }
 
-export const ctpAnonymousClient = new ClientBuilder()
-  .withAnonymousSessionFlow(authAnonymousMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware()
-  .build();
+  private getClient(): Client {
+    return this.userAuthOptions
+      ? userClientBuilder
+          .withProjectKey(this.projectKey)
+          .withPasswordFlow(this.getUserAuthOptions())
+          .withHttpMiddleware(this.getHttpMiddlewareOptions())
+          .withLoggerMiddleware()
+          .build()
+      : anonymousClientBuilder
+          .withProjectKey(this.projectKey)
+          .withAnonymousSessionFlow(this.getAnonymousAuthOptions())
+          .withHttpMiddleware(this.getHttpMiddlewareOptions())
+          .withLoggerMiddleware()
+          .build();
+  }
 
-export const anonymousApiRoot = createApiBuilderFromCtpClient(ctpAnonymousClient).withProjectKey({
-  projectKey: process.env.REACT_APP_PROJECT_KEY as string,
-});
+  public getProjectKey(): string {
+    return this.projectKey;
+  }
+
+  private getUserAuthOptions(): PasswordAuthMiddlewareOptions {
+    return {
+      host: this.oauthURL,
+      projectKey: this.projectKey,
+      credentials: { ...this.credentials, user: this.userAuthOptions as UserAuthOptions },
+      fetch,
+    };
+  }
+
+  private getAnonymousAuthOptions(): AnonymousAuthMiddlewareOptions {
+    return {
+      host: this.oauthURL,
+      projectKey: this.projectKey,
+      credentials: this.credentials,
+      fetch,
+    };
+  }
+
+  private getHttpMiddlewareOptions(): HttpMiddlewareOptions {
+    return {
+      host: this.apiURL,
+      fetch,
+    };
+  }
+
+  public getApiRoot() {
+    return createApiBuilderFromCtpClient(this.getClient()).withProjectKey({ projectKey: this.projectKey });
+  }
+}
+
+export default CtpClient;
