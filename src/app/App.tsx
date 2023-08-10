f3839f1e7101bcd17062e6f6bbe93c320b5278e6
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-08-10 11:20:38 +0300

    feat: add useState for user login state

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,8 +8,11 @@ import LoginPage from '../components/pages/login/LoginPage';
 import RegistrationPage from '../components/pages/registration/RegistrationPage';
 import BasketPage from '../components/pages/basket/BasketPage';
 import NotFoundPage from '../components/pages/not-found/NotFoundPage';
+import { useState } from 'react';
 
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -17,7 +20,7 @@ function App() {
         <Route path="catalog" element={<CatalogPage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="registration" element={<RegistrationPage />} />
+        <Route path="registration" element={<RegistrationPage onSignup={setUserLoggedIn} />} />
         <Route path="basket" element={<BasketPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
