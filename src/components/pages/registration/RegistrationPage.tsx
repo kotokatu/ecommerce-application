a6167703a204053c3b4c2a69b229ee8a27a5a4e7
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-08-11 19:54:25 +0300

    fix: change input validation to ochange

--- src/components/pages/registration/RegistrationPage.tsx
+++ src/components/pages/registration/RegistrationPage.tsx
@@ -73,7 +73,7 @@ const RegistrationPage = ({ onSignin }: RegistrationPageProps) => {
         postalCode: validation.postalCode,
       },
     },
-    validateInputOnBlur: true,
+    validateInputOnChange: true,
   });
 
   const setBillingAddress = (isChecked: boolean) => {
