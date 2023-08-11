2a0034ca683d2607f9cfa5770843326b6a6d3ada
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-08-11 14:24:30 +0300

    refactor: improve validation error messages

--- src/utils/helpers/validation.ts
+++ src/utils/helpers/validation.ts
@@ -10,7 +10,7 @@ export const validation = {
   lastName: (value: string) => (/^[a-zA-Z]+$/.test(value) ? null : 'Last name is too short'),
   dateOfBirth: (value: Date) => (!value || getAge(value) < 13 ? 'Age must be greater than or equal 13' : null),
   country: (value: string) => (value ? null : 'Please choose a country'),
-  city: (value: string) => (/^[a-zA-Z]+$/.test(value) ? null : 'Should be a valid city name'),
+  city: (value: string) => (/^[a-zA-Z]+$/.test(value) ? null : 'Should only contain letters'),
   streetName: (value: string) => (value ? null : 'Address is too short'),
-  postalCode: (value: string) => (/\b\d{5}\b$/.test(value) ? null : 'Should be a valid postal code'),
+  postalCode: (value: string) => (/\b\d{5}\b$/.test(value) ? null : 'Should be a valid postal code (5 digits)'),
 };
