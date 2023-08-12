697348ffa67e96f7c12aa9c508fe9bd36803e986
vikkitorry <vikkitorry6716@gmail.com>
2023-08-12 18:15:58 +1000

    refactor: message

--- src/utils/constants/messages.ts
+++ src/utils/constants/messages.ts
@@ -1,2 +1,2 @@
-export const successfullLoginMessage = 'You have successfully logged';
+export const successfullLoginMessage = 'You have successfully logged in';
 export const unknownErrMessage = 'An unexpected error occurred';
