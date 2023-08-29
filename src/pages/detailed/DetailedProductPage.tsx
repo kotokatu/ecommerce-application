acc88bc0978d5c3dea8ecc09b9c1832e92486bda
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-08-29 19:33:40 +0300

    refactor: refactor styles

--- src/pages/detailed/DetailedProductPage.tsx
+++ src/pages/detailed/DetailedProductPage.tsx
@@ -1,39 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Grid, SimpleGrid, Paper, Button, Select, Title, rem, createStyles } from '@mantine/core';
+import {
+  Container,
+  Grid,
+  SimpleGrid,
+  Paper,
+  Button,
+  Select,
+  Title,
+  rem,
+  getStylesRef,
+  Loader,
+  useMantineTheme,
+} from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
 import { userService } from '../../services/UserService/UserService';
 import { ProductProjection } from '@commercetools/platform-sdk';
 import parse from 'html-react-parser';
 import './detailed-product-page.scss';
 
-const useStyles = createStyles((theme) => ({
-  container: {
-    width: '100%',
-    fontFamily: 'Montserrat',
-  },
-  carousel: {
-    width: '100%',
-    maxWidth: rem(480),
-    margin: 'auto',
-  },
-  slide: {
-    height: rem(600),
-    backgroundPosition: 'center center',
-    backgroundSize: 'contain',
-    backgroundRepeat: 'no-repeat',
-
-    [theme.fn.smallerThan('md')]: {
-      height: rem(500),
-    },
-  },
-}));
-
 const DetailedProductPage = (): JSX.Element => {
-  const { classes } = useStyles();
   const [product, setProduct] = useState<ProductProjection>();
   const { productKey } = useParams();
-
+  const theme = useMantineTheme();
   useEffect(() => {
     const fetchData = async () => {
       const data = await userService.getProduct(productKey as string);
@@ -45,7 +34,7 @@ const DetailedProductPage = (): JSX.Element => {
   const slides = (product: ProductProjection): JSX.Element[] | null => {
     if (product.masterVariant.images) {
       return product?.masterVariant.images.map((image, ind) => (
-        <Carousel.Slide key={'slide' + ind} className={classes.slide} sx={{ backgroundImage: `url(${image.url})` }} />
+        <Carousel.Slide key={'slide' + ind} sx={{ backgroundImage: `url(${image.url})` }} />
       ));
     }
     return null;
@@ -61,10 +50,43 @@ const DetailedProductPage = (): JSX.Element => {
   };
 
   return (
-    <Container className={classes.container} my="md" size="lg">
+    <Container w="100%" my="md" px={0} size="lg">
       {product ? (
         <SimpleGrid cols={2} spacing={40} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-          <Carousel className={classes.carousel} withIndicators>
+          <Carousel
+            styles={{
+              root: {
+                width: '100%',
+                maxWidth: rem(480),
+                margin: 'auto',
+                '&:hover': {
+                  [`& .${getStylesRef('controls')}:not([data-inactive])`]: {
+                    opacity: 1,
+                  },
+                },
+              },
+              control: {
+                ref: getStylesRef('controls'),
+                transition: 'opacity 150ms ease',
+                opacity: 0,
+                '&[data-inactive]': {
+                  opacity: 0,
+                  cursor: 'default',
+                },
+              },
+              slide: {
+                height: rem(650),
+                backgroundPosition: 'center center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+
+                [theme.fn.smallerThan('md')]: {
+                  height: rem(500),
+                },
+              },
+            }}
+            withIndicators
+          >
             {slides(product)}
           </Carousel>
           <Grid gutter="md">
@@ -73,24 +95,23 @@ const DetailedProductPage = (): JSX.Element => {
                 <Title order={4}>
                   {product.masterVariant.attributes?.find((attribute) => attribute.name === 'brand')?.value.label}
                 </Title>
-                <Title order={6} sx={{ fontFamily: 'Montserrat' }}>
+                <Title order={6} ff="Montserrat">
                   {product.name['en-US']}
                 </Title>
-                <Title order={6} mt="md" sx={{ fontFamily: 'Montserrat' }}>
+                <Title order={6} mt="md" ff="Montserrat">
                   {product.masterVariant.prices ? '€' + product.masterVariant.prices[0].value.centAmount / 100 : 0}
                 </Title>
-                <Select mt="md" mb="md" withinPortal data={productSizes(product)} placeholder="Select size" />
+                <Select my="md" maw={450} withinPortal data={productSizes(product)} placeholder="Select size" />
                 <Button>Add To Cart</Button>
-                <Paper sx={{ fontSize: '13px' }} className="product-description">
+                <Paper fz={13} className="product-description" ff="Montserrat">
                   {product?.description?.['en-US'] ? parse(product?.description?.['en-US']) : ''}
                 </Paper>
               </Paper>
             </Grid.Col>
-            <Grid.Col span={12}></Grid.Col>
           </Grid>
         </SimpleGrid>
       ) : (
-        <Title>Loading</Title>
+        <Loader variant="bars" size="xl" display="block" mx="auto" />
       )}
     </Container>
   );
