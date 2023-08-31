7bea279ceb894bd15ad299728b36562fab546a04
Kirich8 <kirichenkomaxim@icloud.com>
2023-08-31 18:14:57 +0300

    feat: update styles BreadCrumbs

--- src/components/catalog/breadcrumbs/BreadCrumbs.tsx
+++ src/components/catalog/breadcrumbs/BreadCrumbs.tsx
@@ -6,12 +6,35 @@ const breadCrumbsStyles = createStyles(() => ({
   links: {
     display: 'flex',
     gap: '10px',
+    paddingBottom: '10px',
   },
 
   link: {
+    position: 'relative',
+    fontSize: '20px',
+
+    '&:last-child': {
+      cursor: 'default',
+    },
+
     '&:not(:last-child)::after': {
       content: '">"',
       marginLeft: '10px',
+    },
+
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      display: 'block',
+      width: '0%',
+      height: '3px',
+      bottom: '0',
+      backgroundColor: 'black',
+      transition: 'width .5s ease 0s',
+    },
+
+    '&:hover:not(:last-child)::before': {
+      width: 'calc(100% - 18px)',
     },
   },
 }));
