6de537213a3b1ccd921ce04b4985f35fb2c9d447
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-09-02 22:54:18 +0300

    fix: fix product search

--- src/components/catalog/input/SearchInput.tsx
+++ src/components/catalog/input/SearchInput.tsx
@@ -1,29 +1,53 @@
-import { Dispatch, SetStateAction, useState } from 'react';
-import { TextInput } from '@mantine/core';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation, useSearchParams } from 'react-router-dom';
+import { TextInput, UnstyledButton } from '@mantine/core';
 import { inputStyles } from './inputStyles';
+import { TbX } from 'react-icons/tb';
 
 type SearchInputProps = {
   label: string;
   placeholder: string;
-  value: string;
-  setValue: Dispatch<SetStateAction<string>>;
-  searchProduct: () => void;
 };
 
-const SearchInput = ({ label, placeholder, value, setValue, searchProduct }: SearchInputProps) => {
+const SearchInput = ({ label, placeholder }: SearchInputProps) => {
   const [focused, setFocused] = useState(false);
-  const { classes } = inputStyles({ floating: value.trim().length !== 0 || focused });
+  const [searchValue, setSearchValue] = useState('');
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { classes } = inputStyles({ floating: searchValue.trim().length !== 0 || focused });
+
+  const setSearchQuery = (query?: string) => {
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set('search', query ?? searchValue);
+    navigate('/catalog?' + searchParams.toString());
+  };
+
+  useEffect(() => {
+    const searchQuery = searchParams.get('search');
+    setSearchValue(searchQuery || '');
+  }, [searchParams]);
 
   return (
     <TextInput
       label={label}
       placeholder={placeholder}
       classNames={classes}
-      value={value}
-      onChange={(event) => setValue(event.currentTarget.value)}
+      value={searchValue}
+      rightSection={
+        <UnstyledButton
+          onClick={() => {
+            setSearchValue('');
+            setSearchQuery('');
+          }}
+        >
+          <TbX />
+        </UnstyledButton>
+      }
+      onChange={(event) => setSearchValue(event.currentTarget.value)}
       onKeyDown={(event) => {
         if (event.code === 'Enter') {
-          searchProduct();
+          setSearchQuery();
         }
       }}
       onFocus={() => setFocused(true)}
