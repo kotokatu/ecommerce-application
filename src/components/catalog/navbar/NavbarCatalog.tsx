3f146b69d15b2acb6ad5c24cab1cd8dfc467d933
Kirich8 <kirichenkomaxim@icloud.com>
2023-09-02 16:50:50 +0300

    feat: add product filter

--- src/components/catalog/navbar/NavbarCatalog.tsx
+++ src/components/catalog/navbar/NavbarCatalog.tsx
@@ -1,9 +1,11 @@
 import { Button, createStyles } from '@mantine/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DropdownLinks from '../dropdown/DropdownLinks';
 import DropdownPrice from '../dropdown/DropdownPrice';
 import DropdownItems from '../dropdown/DropdownItems';
 import { CategoryType } from '../../../pages/catalog/CatalogPage';
+import { FilterParams } from '../../../services/ProductService/ProductService';
+import { useParams } from 'react-router-dom';
 
 const navbarCatalogStyles = createStyles(() => ({
   navbar: {
@@ -32,9 +34,18 @@ type NavbarCatalogProps = {
   colors: string[];
   minProductPrice: number;
   maxProductPrice: number;
+  setFilters: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
-const NavbarCatalog = ({ categories, brands, sizes, colors, minProductPrice, maxProductPrice }: NavbarCatalogProps) => {
+const NavbarCatalog = ({
+  categories,
+  brands,
+  sizes,
+  colors,
+  minProductPrice,
+  maxProductPrice,
+  setFilters,
+}: NavbarCatalogProps) => {
   const { classes } = navbarCatalogStyles();
   const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
   const [selectedSizes, setSelectedSizes] = useState<string[]>([]);
@@ -42,20 +53,20 @@ const NavbarCatalog = ({ categories, brands, sizes, colors, minProductPrice, max
   const [minPrice, setMinPrice] = useState('');
   const [maxPrice, setMaxPrice] = useState('');
   const [priceRange, setPriceRange] = useState([minProductPrice, maxProductPrice]);
+  const { category, subcategory } = useParams();
+
+  useEffect(() => {
+    clearFilterProducts();
+  }, [category, subcategory]);
 
   function getFilterProducts() {
-    console.log(
-      'min',
-      priceRange[0],
-      'max',
-      priceRange[1],
-      'brands',
-      selectedBrands,
-      'sizes',
-      selectedSizes,
-      'colors',
-      selectedColors,
-    );
+    setFilters([
+      category ? `${FilterParams.category}: "${subcategory || category}"` : '',
+      selectedBrands.length ? `${FilterParams.brand}: "${selectedBrands.join('", "')}"` : '',
+      selectedSizes.length ? `${FilterParams.size}: "${selectedSizes.join('", "')}"` : '',
+      selectedColors.length ? `${FilterParams.color}: "${selectedColors.join('", "')}"` : '',
+      minPrice ? `${FilterParams.price}: range(${Number(minPrice) * 100} to ${Number(maxPrice) * 100})` : '',
+    ]);
   }
 
   function clearFilterProducts() {
@@ -66,6 +77,7 @@ const NavbarCatalog = ({ categories, brands, sizes, colors, minProductPrice, max
     setSelectedBrands([]);
     setSelectedSizes([]);
     setSelectedColors([]);
+    setFilters([]);
   }
 
   return (
