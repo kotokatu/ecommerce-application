ac9384376a41b5e24f23928cd30df61adc58faaa
vikkitorry <vikkitorry6716@gmail.com>
2023-09-03 21:28:38 +0700

    refactor: address component

--- src/pages/profile/profileModes/profileAddressForm.tsx
+++ src/pages/profile/profileModes/profileAddressForm.tsx
@@ -1,6 +1,6 @@
-import { Paper, Text, createStyles, Checkbox, Container, Flex, Button, TextInput, Select } from '@mantine/core';
+import { Paper, Text, createStyles, Checkbox, TextInput, Select } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { useState } from 'react';
+//import { useState } from 'react';
 import { Address } from '../../../services/UserService/UserService';
 import { onlyLettersRegex, postalCodeRegex } from '../../../utils/constants/validationRegex';
 
@@ -36,7 +36,14 @@ const countryData = [
   { value: 'FR', label: 'France' },
 ];
 
-const ProfileAddress = (props: { addressData: Address; userEmail: string; isDefault: boolean }) => {
+type Props = {
+  name?: string;
+  addressData: Address;
+  userEmail: string;
+  isDefault: boolean;
+};
+
+const ProfileAddress = (props: Props) => {
   //const [isLoading, setIsLoading] = useState(false);
   const { classes } = formStyles();
   const address: Address = props.addressData;
@@ -62,31 +69,25 @@ const ProfileAddress = (props: { addressData: Address; userEmail: string; isDefa
   return (
     <Paper shadow="xs" withBorder style={{ width: '100%', padding: '0 1rem' }}>
       <Text className={classes.smallTitle} align="center">
-        Billing Address
+        {props.name} Address
       </Text>
       <Select
         withAsterisk
         label="Country"
         data={countryData}
         placeholder="Choose country"
-        {...form.getInputProps('billingAddress.country')}
+        {...form.getInputProps('country')}
       />
       <TextInput withAsterisk pt={10} label="City" placeholder="City" {...form.getInputProps('billingAddress.city')} />
-      <TextInput
-        withAsterisk
-        pt={10}
-        label="Address"
-        placeholder="Address"
-        {...form.getInputProps('billingAddress.streetName')}
-      />
+      <TextInput withAsterisk pt={10} label="Address" placeholder="Address" {...form.getInputProps('streetName')} />
       <TextInput
         withAsterisk
         pt={10}
         label="Postal Code"
         placeholder="Postal Code"
-        {...form.getInputProps('billingAddress.postalCode')}
+        {...form.getInputProps('postalCode')}
       />
-      <Checkbox m={10} label="Set as default billing address" {...form.getInputProps('setDefaultBillingAddress')} />
+      <Checkbox m={10} label="Set as default address" {...form.getInputProps('setDefault')} />
     </Paper>
   );
 };
