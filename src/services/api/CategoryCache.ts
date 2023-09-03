bff40961e3b2238d2561845e69ff1a2e8cb67a11
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-09-03 22:28:12 +0300

    refactor: add try catch to category cache

--- src/services/api/CategoryCache.ts
+++ src/services/api/CategoryCache.ts
@@ -1,4 +1,5 @@
 import { storeService } from '../StoreService/StoreService';
+import { notificationError } from '../../components/ui/notification';
 
 export type CategoryType = {
   id: string;
@@ -15,15 +16,19 @@ class CategoryCache {
   }
 
   async get() {
-    const categories = await storeService.getCategories();
-    if (categories) {
-      this.categories = categories.map((category) => ({
-        id: category.id,
-        name: category.name['en-US'],
-        order: category.orderHint,
-        parentID: category.parent?.obj?.id,
-        parentName: category.parent?.obj?.name['en-US'],
-      }));
+    try {
+      const categories = await storeService.getCategories();
+      if (categories) {
+        this.categories = categories.map((category) => ({
+          id: category.id,
+          name: category.name['en-US'],
+          order: category.orderHint,
+          parentID: category.parent?.obj?.id,
+          parentName: category.parent?.obj?.name['en-US'],
+        }));
+      }
+    } catch (err) {
+      if (err instanceof Error) notificationError(err.message);
     }
   }
 
