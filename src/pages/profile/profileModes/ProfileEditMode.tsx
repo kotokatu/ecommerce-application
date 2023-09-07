948afb5b0ec7eba7995b027df6e5c6942a982182
vikkitorry <vikkitorry6716@gmail.com>
2023-09-07 10:24:35 +0700

    fix: bugs

--- src/pages/profile/profileModes/ProfileEditMode.tsx
+++ src/pages/profile/profileModes/ProfileEditMode.tsx
@@ -27,7 +27,6 @@ const ProfileEdit = (props: { profile: UserProfile; updatePage: () => void; disa
   const { profile, updatePage, disableBtn } = props;
   const { classes } = formStyles();
   const [opened, { open, close }] = useDisclosure(false);
-  //const [isLoading, setIsLoading] = useState(false);
   const [addresses, setAddresses] = useState([...profile.addresses]);
   const [userData, setUserData] = useState({ ...profile });
 
@@ -75,15 +74,15 @@ const ProfileEdit = (props: { profile: UserProfile; updatePage: () => void; disa
               onSubmit={form.onSubmit(async (values) => {
                 console.log(4644646, disableBtn);
                 if (!opened) {
-                  //setIsLoading(true);
                   try {
                     await storeService.updateCurrentCustomer(values, userData.version);
                     notificationSuccess('Account was succesfully updated');
                   } catch (err) {
-                    if (err instanceof Error) notificationError(err.message);
+                    if (err instanceof Error) {
+                      notificationError(err.message);
+                    }
                   } finally {
                     updatePage();
-                    //setIsLoading(false);
                   }
                 }
               })}
@@ -115,7 +114,7 @@ const ProfileEdit = (props: { profile: UserProfile; updatePage: () => void; disa
               />
 
               <Flex align="center" mb={20} justify="space-between">
-                <Text color="dimmed" size="sm" pt={5}>
+                <Text color="dimmed" size="sm" pt={5} pl={12}>
                   Do you want to update personal info?
                 </Text>
                 <Button type="submit" loading={disableBtn} style={{ width: '180px' }}>
@@ -123,7 +122,7 @@ const ProfileEdit = (props: { profile: UserProfile; updatePage: () => void; disa
                 </Button>
               </Flex>
 
-              <Modal opened={opened} onClose={close} title="Change a Password" centered>
+              <Modal opened={opened} onClose={close} centered>
                 <ProfileModal userVersion={userData.version} userEmail={userData.email} />
               </Modal>
             </form>
