bae371c0f446fe0da8ff269b10558317f89b8668
vikkitorry <vikkitorry6716@gmail.com>
2023-09-13 16:04:21 +0700

    feat: add page about us

---
+++ src/pages/about-us/components/developer.tsx
@@ -0,0 +1,64 @@
+import { createStyles, Text, rem } from '@mantine/core';
+import AnimatedDeveloper from './animatedDeveloper';
+
+const useStyles = createStyles((theme) => ({
+  feature: {
+    position: 'relative',
+    paddingTop: rem(0),
+    paddingLeft: rem(0),
+  },
+
+  overlay: {
+    position: 'absolute',
+    height: rem(120),
+    width: rem(180),
+    top: 0,
+    left: 0,
+    zIndex: 1,
+  },
+
+  content: {
+    position: 'relative',
+    zIndex: 2,
+  },
+
+  icon: {
+    color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+  },
+}));
+
+interface DeveloperProps extends React.ComponentPropsWithoutRef<'div'> {
+  name: string;
+  greeting: string;
+  description: string;
+  conclusion: string;
+}
+
+function Developer({ name, greeting, description, className, conclusion, ...others }: DeveloperProps) {
+  const { classes, cx } = useStyles();
+
+  return (
+    <div className={cx(classes.feature, className)} {...others}>
+      <div className={classes.overlay}>
+        <div className={name} />
+      </div>
+      <AnimatedDeveloper name={name} />
+      <div className={classes.content}>
+        <Text fw={700} fz="lg" mb="xs" mt={5} c="white">
+          {name}
+        </Text>
+        <Text c="black" fz="sm">
+          {greeting}
+        </Text>
+        <Text c="dimmed" fz="sm" mt={10}>
+          {description}
+        </Text>
+        <Text c="red" fz="sm" mt={10}>
+          {conclusion}
+        </Text>
+      </div>
+    </div>
+  );
+}
+
+export default Developer;
---
+++ src/pages/about-us/data/data.ts
@@ -0,0 +1,23 @@
+export const developersData = [
+  {
+    greeting: 'Hello!',
+    name: 'Natasha',
+    description:
+      'I first became interested in computer science when I got my first desktop back in 2001, but at that moment I was already a Linguistics major, so I never pursued CS field as a career. After having worked for an international forest industry corporation for 15 years I decided to change my career path to something that feels more satisfying and authentic to me. Becoming a Front-end developer has become my dream and my goal.',
+    conclusion: '',
+  },
+  {
+    greeting: 'Hi!',
+    name: 'Maxim',
+    description: 'Slakoth’s heart beats just once a minute. Whatever happens, it is content to loaf around motionless.',
+    conclusion: '',
+  },
+  {
+    greeting: 'Hey there!',
+    name: 'Victoriia',
+    description:
+      'I started my acquaintance with frontend technologies by studying HTML Academy courses. After passing all simulators, I continued to study by myself and some time later found this amazing courses. My main strengths are perseverance and the desire to obtain new information. I am interested in web development because this profession provides great opportunities for professional growth.',
+    conclusion:
+      "I'm glad that I was able to complete the RSS School courses. Now we need to move on and conquer new heights.",
+  },
+];
