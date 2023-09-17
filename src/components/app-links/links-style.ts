efd067866060b528d2c5beabd402b18b78deb787
Kirich8 <kirichenkomaxim@icloud.com>
2023-09-17 15:01:19 +0300

    feat: add products count, icons, adaptive

--- src/components/app-links/links-style.ts
+++ src/components/app-links/links-style.ts
@@ -3,12 +3,15 @@ import { createStyles, rem } from '@mantine/core';
 export const linksStyle = createStyles((theme) => ({
   link: {
     fontSize: 'initial',
-    display: 'block',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     lineHeight: 1,
-    padding: `${rem(8)} ${rem(12)}`,
+    padding: `${rem(8)} ${rem(22)}`,
     borderRadius: theme.radius.sm,
     textDecoration: 'none',
     color: theme.colors.dark[9],
+    height: '43px',
 
     '&:hover': {
       backgroundColor: theme.colors.gray[2],
@@ -21,6 +24,60 @@ export const linksStyle = createStyles((theme) => ({
   },
 
   linkActive: {
+    '&,&:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'dark', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'dark', color: theme.primaryColor }).color,
+    },
+  },
+
+  user: {
+    cursor: 'pointer',
+    height: '43px',
+    padding: `${rem(8)} ${rem(22)}`,
+    borderRadius: theme.radius.sm,
+    '&:hover': {
+      backgroundColor: theme.colors.gray[2],
+    },
+  },
+
+  icon: {
+    position: 'relative',
+  },
+
+  productcount: {
+    fontSize: '13px',
+    color: 'black',
+    position: 'absolute',
+    top: '13px',
+    left: '24px',
+    backgroundColor: 'pink',
+    borderRadius: '100%',
+    minWidth: '20px',
+    width: 'fit-content',
+    height: '20px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  basket: {
+    fontSize: 'initial',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(22)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: theme.colors.dark[9],
+    height: '43px',
+
+    '&:hover': {
+      backgroundColor: theme.colors.gray[2],
+    },
+  },
+
+  basketActive: {
     '&, &:hover': {
       backgroundColor: theme.fn.variant({ variant: 'dark', color: theme.primaryColor }).background,
       color: theme.fn.variant({ variant: 'dark', color: theme.primaryColor }).color,
